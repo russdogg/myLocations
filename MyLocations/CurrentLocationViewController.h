770d1d1732df1bb454f534653777493e28a7270d
Russ Dahlberg <rdahlberg@gmail.com>
2013-03-05 00:10:03 -0800

    updatd with GPS error checking

--- MyLocations/CurrentLocationViewController.h
+++ MyLocations/CurrentLocationViewController.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+@interface CurrentLocationViewController : UIViewController <
+CLLocationManagerDelegate>
 
-@interface CurrentLocationViewController : UIViewController
+@property (nonatomic,strong) IBOutlet UILabel *messageLabel;
+@property (nonatomic,strong) IBOutlet UILabel *latitudeLabel;
+@property (nonatomic,strong) IBOutlet UILabel *longitudeLabel;
+@property (nonatomic,strong) IBOutlet UILabel *addressLabel;
+@property (nonatomic,strong) IBOutlet UIButton *tagButton;
+@property (nonatomic,strong) IBOutlet UIButton *getButton;
+
+-(IBAction)getLocation:(id)sender;
+
 
 @end
--- MyLocations/CurrentLocationViewController.m
+++ MyLocations/CurrentLocationViewController.m
@@ -9,11 +9,48 @@
 #import "CurrentLocationViewController.h"
 
 @implementation CurrentLocationViewController
+{
+    CLLocationManager *locationManager;
+    CLLocation *location;
+    BOOL updatingLocation;
+    NSError *lastLocationError;
+}
+
+@synthesize messageLabel, longitudeLabel, latitudeLabel, addressLabel, tagButton, getButton;
+
+-(IBAction)getLocation:(id)sender
+{
+    if(updatingLocation)
+    {
+        [self stopLocationManager];
+    }
+    else
+    {
+        location = nil;
+        lastLocationError = nil;
+        [self startLocationManager];
+    }
+    
+    [self updateLabels];
+    [self configureGetButton];
+}
+
+
+- (id) initWithCoder:(NSCoder *)aDecoder
+{
+    if((self = [super initWithCoder:aDecoder]))
+    {
+        locationManager= [[CLLocationManager alloc] init];
+    }
+    return self;
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self updateLabels];
+    [self configureGetButton];
 }
 
 
@@ -22,12 +59,140 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+- (void)updateLabels
+{
+    if (location != nil)
+    {
+        self.messageLabel.text = @"GPS Coordinates";
+        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", location.coordinate.latitude];
+        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", location.coordinate.longitude];
+        self.tagButton.hidden = NO;
+    }
+    else
+    {
+        self.latitudeLabel.text = @"";
+        self.longitudeLabel.text = @"";
+        self.addressLabel.text = @"";
+        self.tagButton.hidden = YES;
+        
+        NSString *statusMessage;
+        if(lastLocationError != nil)
+        {
+            if ([lastLocationError.domain isEqualToString:kCLErrorDomain] && lastLocationError.code == kCLErrorDenied)
+            {
+                statusMessage = @"Location Services Disabled";
+            }
+            else
+            {
+                statusMessage = @"Error Getting Location";
+            }
+        }
+        else if(![CLLocationManager locationServicesEnabled])
+        {
+            statusMessage = @"Location Services Disabled";
+        }
+        else if(updatingLocation)
+        {
+            statusMessage = @"Searching...";
+        }
+        else
+        {
+            statusMessage = @"Press the Button to Start";
+        }
+        
+        self.messageLabel.text = statusMessage;
+        
+    }
+}
 
+-(void)configureGetButton
+{
+    if (updatingLocation)
+    {
+        [self.getButton setTitle:@"Stop" forState:UIControlStateNormal];
+    }
+    else
+    {
+        [self.getButton setTitle:@"Get My Location" forState:UIControlStateNormal];
+    }
+}
+
+-(void)startLocationManager
+{
+    if ([CLLocationManager locationServicesEnabled])
+    {
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        [locationManager startUpdatingLocation];
+        updatingLocation = YES;
+    }
+}
+
+-(void)stopLocationManager
+{
+    if(updatingLocation)
+    {
+        [locationManager stopUpdatingLocation];
+        locationManager.delegate = nil;
+        updatingLocation = NO;
+    }
+}
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError %@", error);
+    if(error.code == kCLErrorLocationUnknown)
+    {
+        return;
+    }
+    [self stopLocationManager];
+    lastLocationError = error;
+    
+    [self updateLabels];
+    [self configureGetButton];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    NSLog(@"didUpdateToLocation %@", newLocation);
+    
+    if ([newLocation.timestamp timeIntervalSinceNow] < -5.0)
+    {
+        return;
+    }
+    
+    if (newLocation.horizontalAccuracy < 0)
+    {
+        return;
+    }
+    
+    if(location == nil || location.horizontalAccuracy > newLocation.horizontalAccuracy)
+    {
+        lastLocationError = nil;
+        location = newLocation;
+        [self updateLabels];
+        
+        if (newLocation.horizontalAccuracy <= locationManager.desiredAccuracy)
+        {
+            NSLog(@"*** We're Done");
+            [self stopLocationManager];
+            [self configureGetButton];
+        }
+    }
+    
+    
 }
 
 @end
+
+
+
+
+
